1d9b617653a97cf68c7e501513d411a6b7372f4a
PabloGonRod <114018509+Pabloyquemas@users.noreply.github.com>
2023-12-19 20:20:08 +0100

    DATOS DE JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataServiceService } from './data-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ob1';
+
+  posts$: Observable<any[]>
+
+  constructor(private dataService: DataServiceService) {
+
+  }
+  ngOnInit(): void {
+    this.posts$ = this.dataService.getPosts()
+  }
 }
